298bab4c5ec173c7371ba87e7e94fb9bb57bd46a
lunmanlee <leemanlun@gmail.com>
2025-12-01 11:51:37 +1300

    add document loader

--- rag.py
+++ rag.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
-from langchain_communit
+from langchain_community.document_loaders import TextLoader
+
 import os
+
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+loader = TextLoader('data/sturgeon_corpus.txt')
+documents = loader.load()

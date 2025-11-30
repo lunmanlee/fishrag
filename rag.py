adf4824ed906fe2f0ef6c0cd5049ff21271ea611
lunmanlee <leemanlun@gmail.com>
2025-12-01 12:25:32 +1300

    use FAISS vector store

--- rag.py
+++ rag.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 import os
 
@@ -13,3 +14,4 @@ documents = loader.load()
 splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 document_chunks = splitter.split_documents(documents)
 embeddings = OpenAIEmbeddings()
+vector_store = FAISS.from_documents(document_chunks, embeddings)

68968d4f062d8fe5a38705124fc136544de6a243
lunmanlee <leemanlun@gmail.com>
2025-12-01 12:09:40 +1300

    add text splitter to chunk the text

--- rag.py
+++ rag.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 import os
 
@@ -8,3 +9,5 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 loader = TextLoader('data/sturgeon_corpus.txt')
 documents = loader.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+document_chunks = splitter.split_documents(documents)
